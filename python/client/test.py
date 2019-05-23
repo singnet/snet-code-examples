5b67c0f76783dfd8ed0ed62124753c1f65799732
Valerio Versace <mail@valerioversace.it>
2019-05-23 14:04:47 +0200

    fixing example to reflect that stub object in service_client has been renamed to service

--- python/client/test.py
+++ python/client/test.py
@@ -14,14 +14,14 @@ service_client = sdk.create_service_client(org_id, service_id, example_service_p
 
 request = example_service_pb2.Numbers(a=20, b=3)
 
-result = service_client.stub.add(request)
+result = service_client.service.add(request)
 print("Performing 20 + 3: {}".format(result))
 
-result = service_client.stub.mul(request)
+result = service_client.service.mul(request)
 print("Performing 20 * 3: {}".format(result))
 
-result = service_client.stub.div(request)
+result = service_client.service.div(request)
 print("Performing 20 / 3: {}".format(result))
 
-result = service_client.stub.sub(request)
+result = service_client.service.sub(request)
 print("Performing 20 - 3: {}".format(result))
