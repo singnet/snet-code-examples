c576c221bab067709f519a78b3be95b890deacd4
Valerio Versace <mail@valerioversace.it>
2019-06-28 18:04:51 +0200

    Switched to new SDK package
    Changed main file to use new package

--- python/client/test.py
+++ python/client/test.py
@@ -1,4 +1,4 @@
-from snet_sdk import SnetSDK
+from snet.sdk import SnetSDK
 
 import example_service_pb2
 import example_service_pb2_grpc
@@ -10,18 +10,23 @@ sdk = SnetSDK(config)
 org_id = "snet"
 service_id = "example-service"
 
-service_client = sdk.create_service_client(org_id, service_id, example_service_pb2_grpc.CalculatorStub)
+service_client = sdk.create_service_client(
+        org_id, service_id, example_service_pb2_grpc.CalculatorStub)
 
 request = example_service_pb2.Numbers(a=20, b=3)
 
+print("Performing {} + {}:".format(request.a, request.b))
 result = service_client.service.add(request)
-print("Performing 20 + 3: {}".format(result))
+print("Result: {}".format(result.value))
 
-result = service_client.service.mul(request)
-print("Performing 20 * 3: {}".format(result))
-
-result = service_client.service.div(request)
-print("Performing 20 / 3: {}".format(result))
-
+print("Performing {} - {}:".format(request.a, request.b))
 result = service_client.service.sub(request)
-print("Performing 20 - 3: {}".format(result))
+print("Result: {}".format(result.value))
+
+print("Performing {} * {}:".format(request.a, request.b))
+result = service_client.service.mul(request)
+print("Result: {}".format(result.value))
+
+print("Performing {} / {}:".format(request.a, request.b))
+result = service_client.service.div(request)
+print("Result: {}".format(result.value))
