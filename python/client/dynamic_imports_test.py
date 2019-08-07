ca7c18ec8532a80fcb92367e0f8cb8f990099edf
Valerio Versace <mail@valerioversace.it>
2019-08-07 00:48:09 -0700

    Added more examples with static and dynamic service client

--- python/client/dynamic_imports_test.py
+++ python/client/dynamic_imports_test.py
@@ -4,13 +4,9 @@ from config import config
 
 sdk = SnetSDK(config)
 
-org_id = "snet"
-service_id = "example-service"
-
-service_client = sdk.create_dynamic_service_client(org_id, service_id)
-
-
 # Examples using the "get_method" utility function
+service_client = sdk.create_dynamic_service_client("snet", "example-service")
+
 method, request_type, _ = service_client.get_method("add")
 request = request_type(a=20, b=3)
 print("Performing {} + {}:".format(request.a, request.b))
@@ -26,11 +22,15 @@ print("Result: {}".format(result.value))
 
 
 # Examples without the get_method utility function
-request = service_client.example_service.message.Numbers(a=17, b=12)
-print("Performing {} - {}:".format(request.a, request.b))
-result = service_client.example_service.service.Calculator.service.sub(request)
-print("Result: {}".format(result.value))
+service_client = sdk.create_dynamic_service_client("snet", "i3d-video-action-recognition")
+request = service_client.message.Input(model="400", url="http://crcv.ucf.edu/THUMOS14/UCF101/UCF101/v_CricketShot_g04_c02.avi")
+print("Performing video action recognition")
+result = service_client.service.VideoActionRecognition.service.video_action_recon(request)
+print("Result: {}".format(result))
 
-print("Performing {} / {}:".format(request.a, request.b))
-result = service_client.example_service.service.Calculator.service.div(request)
-print("Result: {}".format(result.value))
+
+service_client = sdk.create_dynamic_service_client("snet", "cntk-image-recon")
+request = service_client.message.Input(model="ResNet152", img_path="https://www.fiftyflowers.com/site_files/FiftyFlowers/Image/Product/Mini-Black-Eye-bloom-350_c7d02e72.jpg")
+print("Performing image recognition")
+result = service_client.service.Recognizer.service.flowers(request)
+print("Result: {}".format(result))
--- python/client/test.py
+++ python/client/test.py
@@ -3,30 +3,37 @@ from snet.sdk import SnetSDK
 import example_service_pb2
 import example_service_pb2_grpc
 
+import image_recon_pb2
+import image_recon_pb2_grpc
+
+import video_action_recon_pb2
+import video_action_recon_pb2_grpc
+
+
 from config import config
 
 sdk = SnetSDK(config)
 
-org_id = "snet"
-service_id = "example-service"
 
 service_client = sdk.create_service_client(
-    org_id, service_id, example_service_pb2_grpc.CalculatorStub)
-
+    "snet", "example-service", example_service_pb2_grpc.CalculatorStub)
 request = example_service_pb2.Numbers(a=20, b=3)
-
 print("Performing {} + {}:".format(request.a, request.b))
 result = service_client.service.add(request)
 print("Result: {}".format(result.value))
 
-print("Performing {} - {}:".format(request.a, request.b))
-result = service_client.service.sub(request)
-print("Result: {}".format(result.value))
 
-print("Performing {} * {}:".format(request.a, request.b))
-result = service_client.service.mul(request)
-print("Result: {}".format(result.value))
+service_client = sdk.create_service_client(
+    "snet", "i3d-video-action-recognition", video_action_recon_pb2_grpc.VideoActionRecognitionStub)
+request = video_action_recon_pb2.Input(model="400", url="http://crcv.ucf.edu/THUMOS14/UCF101/UCF101/v_CricketShot_g04_c02.avi")
+print("Performing video action recognition")
+result = service_client.service.video_action_recon(request)
+print("Result: {}".format(result))
 
-print("Performing {} / {}:".format(request.a, request.b))
-result = service_client.service.div(request)
-print("Result: {}".format(result.value))
+
+service_client = sdk.create_service_client(
+    "snet", "cntk-image-recon", image_recon_pb2_grpc.RecognizerStub)
+request = image_recon_pb2.Input(model="ResNet152", img_path="https://www.fiftyflowers.com/site_files/FiftyFlowers/Image/Product/Mini-Black-Eye-bloom-350_c7d02e72.jpg")
+print("Performing image recognition")
+result = service_client.service.flowers(request)
+print("Result: {}".format(result))
